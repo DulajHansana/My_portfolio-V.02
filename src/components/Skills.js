d157bcbb8d4f0b8f62613e937621b882edcf7dee
DuLa_1sT <161925626+DulajHansana@users.noreply.github.com>
2025-01-27 00:49:02 +0530

    Update Skills.js to enhance skill descriptions and adjust progress values

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,8 +14,7 @@ const Skills = () => {
             <span>My Skills</span>
             <h3>I Develop Skills Regularly</h3>
             <p>
-              Dliquip ex ea commo do conse namber onequa ute irure dolor in
-              reprehen derit in voluptate
+              I am a full-stack developer use the latest technologies to build a website 
             </p>
           </div>
           <div className="skills_wrapper">
@@ -23,27 +22,12 @@ const Skills = () => {
               <div className="dodo_progress">
                 <div
                   className="skillsInner___ progress_inner"
-                  data-value={95}
+                  data-value={80}
                   data-color="#142eb5"
                 >
                   <span>
-                    <span className="label">WordPress</span>
-                    <span className="number">95%</span>
-                  </span>
-                  <div className="background">
-                    <div className="bar">
-                      <div className="bar_in" />
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="skillsInner___ progress_inner"
-                  data-value={75}
-                  data-color="#142eb5"
-                >
-                  <span>
-                    <span className="label">Joomla</span>
-                    <span className="number">75%</span>
+                    <span className="label">JavaScript</span>
+                    <span className="number">80%</span>
                   </span>
                   <div className="background">
                     <div className="bar">
@@ -57,8 +41,23 @@ const Skills = () => {
                   data-color="#142eb5"
                 >
                   <span>
-                    <span className="label">Drupal</span>
+                    <span className="label">HTML 5</span>
                     <span className="number">90%</span>
+                  </span>
+                  <div className="background">
+                    <div className="bar">
+                      <div className="bar_in" />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="skillsInner___ progress_inner"
+                  data-value={70}
+                  data-color="#142eb5"
+                >
+                  <span>
+                    <span className="label">CSS</span>
+                    <span className="number">70%</span>
                   </span>
                   <div className="background">
                     <div className="bar">
@@ -72,42 +71,42 @@ const Skills = () => {
               <div className="dodo_progress">
                 <div
                   className="skillsInner___ progress_inner"
-                  data-value={70}
-                  data-color="#142eb5"
-                >
-                  <span>
-                    <span className="label">Python</span>
-                    <span className="number">70%</span>
-                  </span>
-                  <div className="background">
-                    <div className="bar">
-                      <div className="bar_in" />
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="skillsInner___ progress_inner"
-                  data-value={80}
-                  data-color="#142eb5"
-                >
-                  <span>
-                    <span className="label">PHP</span>
-                    <span className="number">80%</span>
-                  </span>
-                  <div className="background">
-                    <div className="bar">
-                      <div className="bar_in" />
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="skillsInner___ progress_inner"
                   data-value={60}
                   data-color="#142eb5"
                 >
                   <span>
-                    <span className="label">JavaScript</span>
+                    <span className="label">WordPress</span>
                     <span className="number">60%</span>
+                  </span>
+                  <div className="background">
+                    <div className="bar">
+                      <div className="bar_in" />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="skillsInner___ progress_inner"
+                  data-value={50}
+                  data-color="#142eb5"
+                >
+                  <span>
+                    <span className="label">MongoDB</span>
+                    <span className="number">50%</span>
+                  </span>
+                  <div className="background">
+                    <div className="bar">
+                      <div className="bar_in" />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="skillsInner___ progress_inner"
+                  data-value={70}
+                  data-color="#142eb5"
+                >
+                  <span>
+                    <span className="label">Node.JS</span>
+                    <span className="number">70%</span>
                   </span>
                   <div className="background">
                     <div className="bar">

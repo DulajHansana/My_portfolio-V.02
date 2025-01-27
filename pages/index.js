c78fdac6795d7ff4bd6bd09b60fd193a74fcf5ae
DuLa_1sT <161925626+DulajHansana@users.noreply.github.com>
2025-01-27 18:41:18 +0530

    Add social media icons to header; update copyright information and remove Testimonials component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
 import About from "../src/components/About";
-import Blog from "../src/components/Blog";
 import Contact from "../src/components/Contact";
 import Copyright from "../src/components/Copyright";
 import CounterSection from "../src/components/CounterSection";
@@ -11,7 +10,6 @@ import Portfolio from "../src/components/Portfolio";
 import Process from "../src/components/Process";
 import Service from "../src/components/Service";
 import Skills from "../src/components/Skills";
-// import Testimonials from "../src/components/Testimonials";
 import Head from "next/head";
 import Header from "../src/layouts/Header";
 import Layout from "../src/layouts/Layout";
@@ -20,9 +18,7 @@ import Mouse from "../src/layouts/Mouse";
 import ScrollTop from "../src/layouts/ScrollTop";
 import Home from "../src/components/Home";
 
-const Testimonials = dynamic(() => import("../src/components/Testimonials"), {
-  ssr: false,
-});
+
 const IndexDark = () => {
   useEffect(() => {
     document.querySelector("body").classList.add("dark");
@@ -44,10 +40,7 @@ const IndexDark = () => {
       <Skills />
       <Partners dark />
       <Service />
-      <Testimonials />
-      
       <Contact />
-      <Blog />
       <Copyright />
       <Mouse />
       <ScrollTop />
